4ee4352caac86fcfca4027cd22971a70e19c6b1f
Alain Brown <alain.brown@gmail.com>
2026-03-13 20:16:12 +0000

    feat: add nextjs-basic template

---
+++ templates/nextjs-basic/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: '__PROJECT_NAME__',
+  description: '__DESCRIPTION__',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
